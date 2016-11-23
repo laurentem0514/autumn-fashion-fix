d88135f1c69ac4221a7f3a4208ba692e4bb72f8d
Lauren Templeton <etothel0514@gmail.com>
2016-11-23 12:45:53 -0500

    hardcoded post to test styles

--- autumn-fashion-fix/src/Components/Posts/Posts.jsx
+++ autumn-fashion-fix/src/Components/Posts/Posts.jsx
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
-import { Col } from 'react-bootstrap';
-//import './Post.css';
+import { Col, Image } from 'react-bootstrap';
+import './Posts.css';
 
 
 class Post extends Component {
@@ -14,14 +14,16 @@ class Post extends Component {
       <Col xs={12} md={4}>
        <div className="post">
 
-        "text": "Seen on the catwalks at Chanel, Ralph Lauren, and Topshop Unique; think luscious icy bright pastels to add a soft pop of colour to the dull winter months. It works beautifully against grey tailoring and comes in a variety of textures from sugar plum boucle to cashmere soft duck egg blue\u2026",
-        "link": "http:\/\/www.bullring.co.uk\/news\/fashion\/aff-edit-new-pastels",
-        "link_text": "Click here to explore the trend.",
-        "image_url": "http://placehold.it/600x350"
+        <Image src="https://media.giphy.com/media/3o8doNAGKZXsrsgzW8/giphy.gif" rounded />
+        <a href="#">link</a>
+        <p>kjhgjlfghxldgh</p>
 
        </div>
 
       </Col>
+
+
+
     );
   }
 }
